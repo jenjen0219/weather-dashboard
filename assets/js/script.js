35249fcf9a1273d07d8655eb3e2eed6eaadaaf53
[Jen ny] <[jengutierrez.1210@gmail.com]>
2023-04-11 20:10:17 -0700

    now have working api's and the functions to grab the information we need

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,41 @@
 var APIKey = '4ef3772979be6dce53798914fca77969';
-var city;
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-console.log(queryURL);
 
-// fetch(queryURL)
-// .then(response => response,json())
-// .then( data => {
+// var queryURLForecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
 
-// })
 
-// forecast.humidity.value
+// target form elements
+var searchFormBtn = document.getElementById("searchCityEl")
+var userInput = document.getElementById('cityName');
 
-//http://api.openweathermap.org/data/2.5/weather?q=seattle&appid=4ef3772979be6dce53798914fca77969
+function handleUserInput(event){
+    event.preventDefault()
+   currentWeather(userInput.value)
+   getForecast(userInput.value)
+
+}
+
+
+function currentWeather(city){
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+    fetch(queryURL).then(function(response){
+        return response.json()
+    }).then(function(data){
+        console.log(data);
+    })
+}
+
+function getForecast(city){
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+
+    fetch(queryURL).then(function(response){
+        return response.json()
+    }).then(function(data){
+        console.log(data);
+    })
+}
+
+
+
+searchFormBtn.addEventListener('submit', handleUserInput)
