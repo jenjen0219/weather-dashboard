de81b72320e2ada7887966571a91556105e3aa6c
[Jen ny] <[jengutierrez.1210@gmail.com]>
2023-05-04 14:07:21 -0700

    the current weather forecast is finally complete and outputting all the correct data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 var APIKey = '7b91ed8314f4e3bf476bcd5d074d7fd6';
 // create javascript variables which make reference to your html elements
+var cityNameTitle = document.getElementById('cityNameTitle');
 var humidityEl = document.getElementById('humidityEl');
 var weatherIcon = document.getElementById('weatherIcon');
 var windEl = document.getElementById('windEl');
 var weatherIconDescription = document.getElementById('weatherIconDescription');
-var forecastCards = Array.from(document.getElementsByClassName('column box'));
-console.log('forecastCards', forecastCards);
-console.log('hey', forecastCards[0].childNodes);
-
+var tempEl = document.getElementById('tempEl');
+var forecastCardParent = document.getElementById('forecast-cards');
+var userSearchHistory = JSON.parse(localStorage.getItem('history')) || [];
 // var queryURLForecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
 
 
@@ -19,6 +19,7 @@ function handleUserInput(event) {
     console.log('submit');
     event.preventDefault()
     getLatAndLon(userInput.value)
+
     //getOneCallWeather(userInput.value)
 }
 
@@ -32,42 +33,83 @@ function getLatAndLon(city) {
         console.log(data);
         var lon = data[0].lon;
         var lat = data[0].lat;
-        console.log(lat, lon);
-        getOneCallWeather(lat, lon)
+        getCallWeather(lat, lon);
+        fiveDayForecast(lat, lon);
+        cityHistory(data[0].name);
     })
 }
 
 
-function getOneCallWeather(lat, lon) {
+function getCallWeather(lat, lon) {
     console.log('inside getonecallweather');
 
-    var queryURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=[minutely,hourly,alerts]&appid=${APIKey}`;
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`;
 
     fetch(queryURL).then(function (response) {
         return response.json()
     }).then(function (data) {
-        var iconCode = data.current.weather[0].icon;
-        console.log(data);
-
-        weatherIconDescription.textContent = `${data.current.weather[0].description.substring(0, 1).toUpperCase()}${data.current.weather[0].description.substring(1)}`;
-
-        humidityEl.textContent = 'Humidity: ' + data.current.humidity;
-
-        windEl.textContent = 'Wind Speed: ' + data.current.wind_speed;
-
+        //grabbing the icon code that we will use to present the icon itself in just a bit for the variable weatherIcon
+        var iconCode = `${data.weather[0].icon}`;
+        weatherIconDescription.textContent = `${data.weather[0].description.substring(0, 1).toUpperCase()}${data.weather[0].description.substring(1)}`;
+        cityNameTitle.textContent = `${data.name}`;
+        humidityEl.textContent = `Humidity: ${data.main.humidity}%`;
+        windEl.textContent = `Wind Speed: ${data.wind.speed} mph`;
         weatherIcon.src = 'https://openweathermap.org/img/wn/' + iconCode + '@2x.png';
-
-        // more assignments of values to textContent properties...
-        // ...
-        // ...
-        document.querySelector('#fiveDayForecast > section').classList.remove("hide");
-        for (let i = 0; i < 5; i++) {
-            //console.log(forecastCards[i].childNodes[1];
-        }
+        tempEl.textContent = `Temperature: ${data.main.temp}`
 
     })
 }
 
+function fiveDayForecast(lat, lon) {
+    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`;
+    fetch(queryURL).then(function (response) {
+        return response.json()
+    }).then(function (data) {
+        // var iconCode = data.current.weather[0].icon;
+        console.log(data);
+
+        // weatherIconDescription.textContent = `${data.current.weather[0].description.substring(0, 1).toUpperCase()}${data.current.weather[0].description.substring(1)}`;
+
+        // humidityEl.textContent = 'Humidity: ' + data.current.humidity;
+
+        // windEl.textContent = 'Wind Speed: ' + data.current.wind_speed;
+
+        // weatherIcon.src = 'https://openweathermap.org/img/wn/' + iconCode + '@2x.png';
+
+        // // more assignments of values to textContent properties...
+        // // ...
+        // // ...
+        // document.querySelector('#fiveDayForecast > section').classList.remove("hide");
+        for (let i = 0; i < 5; i++) {
+            // create the cards
+            // const child = document.createElement('div');
+            // const child = document.createElement('div');
+            // const child = document.createElement('div');
+
+            // child.className = 'card';
+            // child.textContent = `This is card ${i + 1}`;
+
+
+            // forecastCardParent.appendChild(child);
+            // add the contents to the cards
+
+            // append the cards to the page
+        }
+
+    })
+
+}
+
+function cityHistory(cityName) {
+    userSearchHistory.push(cityName);
+    localStorage.setItem('history', JSON.stringify(userSearchHistory));
+    createBtns()
+}
+
+function createBtns() {
+    // run a forloop that will take the length of the userSearch history. Have it create a button for every item inside the array. append that item to the element with the id forecast-cards
+
+}
 
 
 searchFormBtn.addEventListener('submit', handleUserInput)
